5b3a2f4c9828128f4e4a1b857b0d7d6429566dd4
Manuel Nevado Fabian <manuneva@ucm.es>
2023-05-29 10:56:15 +0200

    Agrupacion de expresiones compuestas en una sola

--- Compilador/src/asint/ExpCompuesta.java
+++ Compilador/src/asint/ExpCompuesta.java
@@ -1,5 +1,31 @@
 package asint;
 
-public class ExpCompuesta extende{
-
+public class ExpCompuesta extends Exp{
+	private Exp e0;
+	private Exp e1;
+	private TipoExpresion tipo;
+	private int prioridad;
+	
+	public ExpCompuesta(Exp e0, Exp e1, TipoExpresion t) {
+		this.e0 = e0;
+		this.e1 = e1;
+		this.tipo = t;
+		//Construir la prioridad aqui
+		switch (t){
+			case SUMA:
+				this.prioridad = 0;
+			case RESTA:
+				//TODO
+		}
+				
+	}
+	
+	public int prioridad() {
+		return 0;
+	}
+	
+	public void procesa(Procesamiento procesamiento) {
+	}
+	
+	
 }
